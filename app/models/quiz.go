3b8864c31fc55ba1bf82b23f71414e3294c98937
HANNACHI-Seif <seiferrahmaneh@gmail.com>
2024-05-18 16:19:07 +0100

    schema slight changes

--- app/models/quiz.go
+++ app/models/quiz.go
@@ -31,11 +31,11 @@ type Question struct {
 	Score       float64            `json:"score" bson:"score" validate:"required" binding:"required"`
 	Image       string             `json:"image" bson:"image"`
 	Options     []Option           `json:"options" bson:"options" validate:"required" binding:"required"`
-	CorrectIdxs []int              `json:"correct_idxs" bson:"correct_idxs" validate:"required" binding:"required"`
+	CorrectIdxs []string              `json:"correct_idxs" bson:"correct_idxs" validate:"required" binding:"required"`
 }
 
 type Option struct {
-	ID 		int 	`json:"id" bson:"id"`
+	ID 		string 	`json:"id" bson:"id"`
 	Option 	string 	`json:"option" bson:"option" validate:"required" binding:"required"` 
 }
 
@@ -47,10 +47,10 @@ type Grade struct {
 
 type Answer struct {
 	QuestionId int 				 `json:"question_id" bson:"question_id" validate:"required" binding:"required"`
-	Choices    []int             `json:"choices" bson:"choices" validate:"required" binding:"required"`
+	Choices    []string             `json:"choices" bson:"choices" validate:"required" binding:"required"`
 	IsCorrect  bool              `json:"is_correct" bson:"is_correct"`
 }
-
+// choices will be compared against correctIdxs in the question
 type Submission struct {
 	ID        primitive.ObjectID `json:"id" bson:"_id"`
 	StudentId string             `json:"student_id" bson:"student_id"`
